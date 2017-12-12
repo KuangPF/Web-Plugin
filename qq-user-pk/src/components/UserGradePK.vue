e1142cd5d997708479489d8c0974bd4f6a2defeb
kuangpengfei <1633397286@qq.com>
2017-12-13 00:07:18 +0800

    progress

--- qq-user-pk/src/components/UserGradePK.vue
+++ qq-user-pk/src/components/UserGradePK.vue
@@ -3,13 +3,15 @@
       <div class="head-content">
           <div class="user">
               <div class="avatar"></div>
-              <div class="nickName">多特蒙德</div>
+              <div class="nickName" v-if="nickNameShow">多特蒙德</div>
               <div class="txtPK" :class="{txtPKX:isStart}">P</div>
+              <div class="progress-bar"><div class="progress-value" :style="{width:progressValueUser}"></div></div>
           </div>
           <div class="me">
               <div class="avatar"></div>
-              <div class="nickName">广州恒大</div>
+              <div class="nickName" v-if="nickNameShow">广州恒大</div>
               <div class="txtPK" :class="{txtPKX:isStart}">K</div>
+              <div class="progress-bar"><div class="progress-value" :style="{width:progressValueMe}"></div></div>
           </div>
       </div>
   </div>
@@ -17,17 +19,24 @@
 
 <script>
 export default {
-    data () {
-        return {
-            isStart:false,
-        }
-    },
-    mounted () {
-        let _this = this;
-        setTimeout(function() {
-            _this.isStart = true;
-        },0)
-    }
+  data() {
+    return {
+      nickNameShow: false,
+      isStart: false,
+      progressValueUser: 0,
+      progressValueMe: 0
+    };
+  },
+  mounted() {
+    let _this = this;
+    setTimeout(function() {
+      _this.isStart = true;
+    }, 0);
+    setTimeout(function() {
+      _this.progressValueUser = '50px';
+      _this.progressValueMe = '60px';
+    }, 0);
+  }
 };
 </script>
 
