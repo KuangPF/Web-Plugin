95216815fb4bc807b8751aa2519dd5eba919c89d
kuangpengfei <kuangpengfei@youloft.com>
2017-12-12 18:50:29 +0800

    qq-user-pk init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 #### 滑动删除       /slide-delete
+#### QQ用户等级PK动画       /qq-user-pk
---
+++ qq-user-pk/src/components/UserGradePK.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="index">
+      <div class="head-content">
+          <div class="user">
+              <div class="avatar"></div>
+              <div class="nickName">多特蒙德</div>
+              <div class="txtPK" :class="{txtPKX:isStart}">P</div>
+          </div>
+          <div class="me">
+              <div class="avatar"></div>
+              <div class="nickName">广州恒大</div>
+              <div class="txtPK" :class="{txtPKX:isStart}">K</div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            isStart:false,
+        }
+    },
+    mounted () {
+        let _this = this;
+        setTimeout(function() {
+            _this.isStart = true;
+        },0)
+    }
+};
+</script>
+
+<style>
+
+</style>
