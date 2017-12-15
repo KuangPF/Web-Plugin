9ee2b34143f87eda715180695f1b44658d8f3a59
kuangpengfei <kuangpengfei@youloft.com>
2017-12-15 16:40:50 +0800

    update

--- src/api/aip.js
+++ src/api/aip.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export function getPluginItems() {
-    return axios.request('../../data.json',{
+    return axios.request('../../static/data/data.json',{
        method:'get',
        params:''
     })
