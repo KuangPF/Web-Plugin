cebb0bd6e9c67157bdbb8af08ed8940e28904f5a
kuangpengfei <1633397286@qq.com>
2018-04-05 23:39:04 +0800

    优化页面切换

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-      <transition>
-        <router-view/>
-      </transition>
+    <transition :name="transitionName">
+      <router-view class="view" />
+    </transition>
   </div>
 </template>
 <script>
@@ -10,12 +10,43 @@ import './sass/style.scss';
 export default {
   name: 'app',
   data() {
-    return {};
+    return {
+      transitionName: ''
+    };
+  },
+  watch: {
+    '$route'(to, from) {
+      const toMeta = to.meta;
+      const fromMeta = from.meta;
+      this.transitionName = toMeta < fromMeta ? 'slide-right' : 'slide-left';
+    }
   }
 };
 </script>
 <style>
 #app {
-    width: 100% !important;
+  width: 100%;
+}
+.view {
+  width: 100%;
+  position: absolute;
+  transition: all 0.3s ease-in-out;
+}
+.slide-right-enter {
+  opacity: 0;
+  transform: translate3d(-100%, 0, 0);
+}
+.slide-right-leave-active {
+  opacity: 0;
+  transform: translate3d(0, 0, 0);
+}
+
+.slide-left-enter {
+  opacity: 0;
+  transform: translate3d(100%, 0, 0);
+}
+.slide-left-leave-active {
+  opacity: 0;
+  transform: translate3d(0, 0, 0);
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import index from '@/components/index';
-import PageTransition from '@/components/PageTransition';
 import SlideDelete from '@/components/SlideDelete';
 /* import HelloWorld from '@/components/HelloWorld'; */
 import PhotoSwipe from '@/components/PhotoSwipe';
@@ -14,51 +13,50 @@ import EamlessScroll from '@/components/EamlessScroll';
 /* test */
 
 Vue.use(Router);
-Router.prototype.goBack = function () {
-  this.isBack = true;
-  window.history.go(-1);
-};
 export default new Router({
   routes: [{
-    path: '/',
-    name: '',
-    component: PageTransition,
-    children: [{
-        path: '',
-        name: 'index',
-        component: index
-      }, {
-        path: '/slide-delete',
-        name: 'SlideDelete',
-        component: SlideDelete
-      }, {
-        path: '/photoswipe',
-        name: 'PhotoSwipe',
-        component: PhotoSwipe
-      }, {
-        path: '/touchmove',
-        name: 'Touchmove',
-        component: Touchmove
-      },
-      {
-        path: '/scroll-linkage',
-        name: 'ScrollLinkage',
-        component: ScrollLinkage
-      },
-      {
-        path: '/test',
-        name: 'Test',
-        component: Test
-      },
-      {
-        path: '/lazy-load',
-        name: 'VueLazyLoad',
-        component: VueLazyLoad
-      }, {
-        path: '/eamless-scroll',
-        name: 'EamlessScroll',
-        component: EamlessScroll
-      }
-    ]
-  }]
+      path: '',
+      name: 'index',
+      component: index,
+      meta: 1
+    }, {
+      path: '/slide-delete',
+      name: 'SlideDelete',
+      component: SlideDelete,
+      meta: 2
+    }, {
+      path: '/photoswipe',
+      name: 'PhotoSwipe',
+      component: PhotoSwipe,
+      meta: 2
+    }, {
+      path: '/touchmove',
+      name: 'Touchmove',
+      component: Touchmove,
+      meta: 2
+    },
+    {
+      path: '/scroll-linkage',
+      name: 'ScrollLinkage',
+      component: ScrollLinkage,
+      meta: 2
+    },
+    {
+      path: '/test',
+      name: 'Test',
+      component: Test,
+      meta: 2
+    },
+    {
+      path: '/lazy-load',
+      name: 'VueLazyLoad',
+      component: VueLazyLoad,
+      meta: 2
+    }, {
+      path: '/eamless-scroll',
+      name: 'EamlessScroll',
+      component: EamlessScroll,
+      meta: 2
+    }
+  ]
 });
