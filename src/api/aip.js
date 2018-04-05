57f605631eb5db7335e0f6d9bd4865ddc929aa82
kuangpengfei <1633397286@qq.com>
2018-04-05 14:51:13 +0800

    update api

--- src/api/aip.js
+++ src/api/aip.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const debug = process.env.NODE_ENV !== 'production';
-const url = debug ? '/api/index' : 'https://kuangpf.github.io/Web-Plugin/dist/static/data/data.json';
+const url = debug ? '/api/index' : 'https://kuangpf.com/Web-Plugin/dist/static/data/data.json';
 export function getPluginItems() {
   return axios.request(url, {
     method: 'get',
